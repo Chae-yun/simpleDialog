2886bf22906696eb2160cdc7b49d4f6aee053a0d
Chaeyun Yang <ycy6685@naver.com>
2016-06-07 20:45:59 +0900

    #4 Change List Dialog

--- app/src/main/java/kr/hs/emirim/ycy6685/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ycy6685/simpledialog/MainActivity.java
@@ -10,10 +10,13 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button but;
+    String[] items={"매콤","상콤","달콤","고소","바삭"};
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //이 문장 호출 후에 리니어레이아웃, 버튼 이런거 다 메모리에 올라감
-        Button but=(Button)findViewById(R.id.but_dialog);
+        but=(Button)findViewById(R.id.but_dialog);
         but.setOnClickListener(this);
     }
 
@@ -27,7 +30,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
         //원래는 쓰이는 액티비티의 객체명.this인데 이건 여기서 쓸거니까 this OK
         dialog.setTitle("First Dialog");
-        dialog.setMessage("This is message part.");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) { //which가 배열의 인덱스를 알아
+                but.setText(items[which]);
+            }
+        });
         dialog.setIcon(R.drawable.first_icon);
         //dialog.setPositiveButton("확인",null); //우리는 창만 닫히고 처리 안할 거니까 널!
         dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
