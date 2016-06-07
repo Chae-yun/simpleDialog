7e9b3741790a69a124e80ad8c783ef478b40f77e
Chaeyun Yang <ycy6685@naver.com>
2016-06-07 19:52:43 +0900

    #2 Create AlertDialog Builder

--- app/src/main/java/kr/hs/emirim/ycy6685/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ycy6685/simpledialog/MainActivity.java
@@ -1,13 +1,32 @@
 package kr.hs.emirim.ycy6685.simpledialog;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //이 문장 호출 후에 리니어레이아웃, 버튼 이런거 다 메모리에 올라감
+        Button but=(Button)findViewById(R.id.but_dialog);
+        but.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        //원래는 쓰이는 액티비티의 객체명.this인데 이건 여기서 쓸거니까 this OK
+        dialog.setTitle("First Dialog");
+        dialog.setMessage("This is message part.");
+        dialog.setIcon(R.drawable.first_icon);
+        dialog.setPositiveButton("확인",null); //우리는 창만 닫히고 처리 안할 거니까 널!
+        dialog.show();
     }
 }
